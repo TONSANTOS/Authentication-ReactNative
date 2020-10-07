31528cf2ead361bd4f08989440fd40ea5184ddd2
Ton Santos <toncotias@icloud.com>
2020-10-06 21:27:20 -0300

    rising application

---
+++ authrn/src/pages/SignIn/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Button, StyleSheet} from 'react-native';
+import {useAuth} from '../../contexts/auth';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const SignIn: React.FC = () => {
+  const {signIn} = useAuth();
+
+  function handleSign() {
+    signIn();
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Sign In" onPress={handleSign} />
+    </View>
+  );
+};
+
+export default SignIn;
